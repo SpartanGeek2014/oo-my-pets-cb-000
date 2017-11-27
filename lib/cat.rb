4f30b347d58a7f76ea0f4b144c71d18cdc25525c
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-27 18:10:10 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,9 +2,9 @@ class Cat
   attr_reader :name
   attr_accessor :mood
   # code goes here
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
-  end
+    def initialize(name)
+      @name = name
+      @mood = "nervous"
+    end
 
 end
